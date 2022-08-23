91782e0bf3cd3db087a5b33cbca38557decb49f5
Shubhammadgical <shubham.chaurasia@madgicaltechdom.com>
2022-08-23 17:55:34 +0530

    UseContext

--- src/UseContext.jsx
+++ src/UseContext.jsx
@@ -2,9 +2,11 @@ import React, { createContext, useState, useContext } from "react";
 const UserContext = createContext();
 function Component1() {
   const [user, setUser] = useState("Jesse Hall");
-
+  const update = (name) => {
+    setUser(name);
+  };
   return (
-    <UserContext.Provider value={user}>
+    <UserContext.Provider value={{ user, update }}>
       <h1>Component 1</h1>
       <h2>{`Hello ${user}!`}</h2>
       <Component2 />
@@ -21,10 +23,12 @@ function Component2() {
 }
 
 function Component3() {
-  const [user, setUser] = useState("Shubham");
+  const data = useContext(UserContext);
+  data.update("Shubham");
   return (
     <>
       <h1>Component 3</h1>
+      <h2>{`Hello ${data.user}!`}</h2>
       <Component4 />
     </>
   );
@@ -40,12 +44,12 @@ function Component4() {
 }
 
 function Component5() {
-  const user = useContext(UserContext);
-
+  const data = useContext(UserContext);
+  data.update("Shubham Chaurasia");
   return (
     <>
       <h1>Component 5</h1>
-      <h2>{`Hello ${user} again!`}</h2>
+      <h2>{`Hello ${data.user} again!`}</h2>
     </>
   );
 }
