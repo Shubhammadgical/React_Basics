ed93a19d076f6925c2ad97c0b4b50cea6c7e5d50
Shubhammadgical <shubham.chaurasia@madgicaltechdom.com>
2022-08-25 13:37:04 +0530

    Implement functionality in Quiz

--- src/QuestionBank.jsx
+++ src/QuestionBank.jsx
@@ -2,7 +2,6 @@ import { eventWrapper } from "@testing-library/user-event/dist/utils";
 import React, { useState } from "react";
 class Quizz extends React.Component {
   state = {
-    showQuestion: false,
     AllQuestions: [],
     totalScore: 0,
     tamplate: {
@@ -12,19 +11,25 @@ class Quizz extends React.Component {
       showOptions: false,
     },
     index: -1,
+    optIndex: -1,
     error: "",
-    NoOfOptions: [],
   };
   handleQues = () => {
     let s1 = this.state;
     s1.showQuestion = true;
-    s1.index++;
-    let NewTemplate = { que: "", score: "", option: [], showOptions: false };
-    if (s1.totalScore < 20) {
+    let NewTemplate = {
+      que: "",
+      score: "",
+      option: [{ opt: "" }, { opt: "" }, { opt: "" }, { opt: "" }],
+
+      NoOfOptions: [],
+    };
+    if (s1.totalScore < 20 && s1.error === "") {
       s1.AllQuestions.push(NewTemplate);
+    } else {
+      s1.error = "Total score is greater than or equal than 20";
     }
     this.setState(s1);
-    console.log(s1.totalScore);
   };
   handleChange = (e) => {
     let s1 = this.state;
@@ -34,16 +39,24 @@ class Quizz extends React.Component {
     if (e.currentTarget.name === "score") {
       s1.AllQuestions[s1.index].score = e.currentTarget.value;
     }
+    if (e.currentTarget.name === "option") {
+      s1.AllQuestions[s1.index].option[s1.optIndex].opt = e.currentTarget.value;
+    }
     this.setState(s1);
   };
   set = () => {
     let s1 = this.state;
+    s1.totalScore = 0;
+    let total = 0;
     if (s1.AllQuestions.length > 0) {
       for (let i = 0; i < s1.AllQuestions.length; i++) {
-        if (s1.totalScore + +s1.AllQuestions[i].score < 20) {
+        total = total + +s1.AllQuestions[i].score;
+        console.log(total);
+        if (s1.totalScore + +s1.AllQuestions[i].score <= 20) {
           s1.totalScore = s1.totalScore + +s1.AllQuestions[i].score;
           s1.error = "";
-        } else {
+        }
+        if (total > 20) {
           s1.error = "Total score is greater than or equal than 20";
         }
       }
@@ -51,16 +64,21 @@ class Quizz extends React.Component {
     this.setState(s1);
   };
   handleOption = (index) => {
-    console.log(index);
     let s1 = this.state;
     s1.AllQuestions[index].showOptions = true;
-    s1.NoOfOptions.push(1);
+    if (s1.AllQuestions[index].NoOfOptions.length < 4) {
+      s1.AllQuestions[index].NoOfOptions.push(1);
+    }
+    this.setState(s1);
+  };
+  handleIndex = (index, optindex) => {
+    let s1 = this.state;
+    s1.index = index;
+    s1.optIndex = optindex;
     this.setState(s1);
   };
   render() {
-    let { totalScore, showQuestion, AllQuestions, error, NoOfOptions } =
-      this.state;
-
+    let { totalScore, AllQuestions, error } = this.state;
     return (
       <div style={{ padding: "20px" }}>
         <div
@@ -70,14 +88,14 @@ class Quizz extends React.Component {
             height: "100px",
           }}
         >
-          <b>World Quiz</b>
-          <div style={{ float: "right", paddingTop: "30px" }}>
+          <h3>World Quiz</h3>
+          <div style={{ float: "right" }}>
             <button className="btn btn-primary" onClick={this.handleQues}>
               Add Question
             </button>
           </div>
           <div
-            style={{ float: "right", paddingTop: "36px", paddingRight: "20px" }}
+            style={{ float: "right", paddingTop: "10px", paddingRight: "20px" }}
           >
             Total Score : {totalScore}
           </div>
@@ -87,13 +105,13 @@ class Quizz extends React.Component {
         </div>
 
         <br />
-        {showQuestion === true && AllQuestions.length > 0 ? (
+        {AllQuestions.length > 0 ? (
           <div>
             {AllQuestions.map((que, index) => (
               <div
                 style={{
                   border: "1px solid black",
-                  padding: "20px",
+                  padding: "10px",
                   marginBottom: "20px",
                 }}
                 key={index}
@@ -103,6 +121,7 @@ class Quizz extends React.Component {
                   placeholder="Question Title"
                   value={que.que}
                   onChange={this.handleChange}
+                  onMouseOver={() => this.handleIndex(index)}
                   name="question"
                 ></input>
                 <br />
@@ -112,16 +131,26 @@ class Quizz extends React.Component {
                   placeholder="Score"
                   value={que.score}
                   onChange={this.handleChange}
+                  onMouseOver={() => this.handleIndex(index)}
                   name="score"
                 ></input>
                 <br />
                 <br />
                 <div>
-                  {que.showOptions && NoOfOptions.length > 0 ? (
+                  {que.NoOfOptions.length > 0 ? (
                     <div>
-                      {NoOfOptions.map((opt) => (
-                        <div>
-                          <input type="text" placeholder="Enter Option"></input>
+                      {que.NoOfOptions.map((opt, optindex) => (
+                        <div key={optindex}>
+                          <input
+                            type="text"
+                            onMouseOver={() =>
+                              this.handleIndex(index, optindex)
+                            }
+                            onChange={this.handleChange}
+                            placeholder="Enter Option"
+                            name="option"
+                            value={que.option[optindex].opt}
+                          ></input>
                           <br />
                           <br />
                         </div>
@@ -135,7 +164,6 @@ class Quizz extends React.Component {
                   style={{
                     float: "right",
                     paddingTop: "25px",
-                    paddingRight: "20px",
                   }}
                 >
                   <button
@@ -147,7 +175,9 @@ class Quizz extends React.Component {
                 </div>
                 <div>
                   <br />
-                  <button onClick={this.set}>Set</button>
+                  <button className="btn btn-success" onClick={this.set}>
+                    Set Score
+                  </button>
                 </div>
               </div>
             ))}
