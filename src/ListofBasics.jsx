d644995b1edd3f9a1e4c1093d0907a32673cd35c
Shubhammadgical <shubham.chaurasia@madgicaltechdom.com>
2022-08-26 16:52:10 +0530

    changes

--- src/ListofBasics.jsx
+++ src/ListofBasics.jsx
@@ -4,26 +4,26 @@ function Concepts() {
   return (
     <div style={{ padding: "20px" }}>
       <h1>Basic Concepts of React </h1>
-      <a href="/fncomp">
-        <h2>Function Component </h2>
-      </a>
-      <a href="/classcomp">
-        <h2>Class Component </h2>
-      </a>
       <a href="/hooks">
         <h2>Hooks </h2>
-      </a>
-      <a href="/jsx">
-        <h2>JSX </h2>
       </a>
       <a href="/multicomp">
         <h2>Multi Component </h2>
       </a>
+      <a href="/redux">
+        <h2>Redux </h2>
+      </a>
       <a href="/usecontext">
         <h2>UseContext </h2>
       </a>
-      <a href="/redux">
-        <h2>Redux </h2>
+      <a href="/classcomp">
+        <h2>Class Component </h2>
+      </a>
+      <a href="/fncomp">
+        <h2>Function Component </h2>
+      </a>
+      <a href="/jsx">
+        <h2>JSX </h2>
       </a>
       <a href="/clickcounter">
         <h2>Higher-Order-Component </h2>
